376cec878cc7db477aa8f5c432eadff115f85397
ShunBai <shunbai@infinite.net.cn>
2017-11-21 16:19:24 +0800

    study 1-10

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vuejs-study
 learning vuejs 2 for myself
+
+[Vue JS 2 Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa)
---
+++ app.js
@@ -0,0 +1,53 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    name: '',
+    job: 'fe',
+    website: 'http://www.whidy.net',
+    websiteTag: '<a href="http://www.whidy.net">my blog(output by v-html)</a>',
+    age: 20,
+    x: 0,
+    y: 0,
+    a: 0,
+    b: 0
+  },
+  methods: {
+    greet: function (time) {
+      return 'Good ' + time + ' ' + this.name;
+    },
+    add: function (inc) {
+      this.age += inc;
+    },
+    subtract: function (dec) {
+      this.age -= dec;
+    },
+    updateXY: function (event) {
+      // console.log(event);
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+    logName: function () {
+      // console.log('you entered your name');
+      // this.name = 
+    },
+    logAge: function () {
+      console.log('you entered your age');
+    },
+    // addToA: function () {
+    //   return this.a + this.age;
+    // },
+    // addToB: function () {
+    //   return this.b + this.age;
+    // }
+  },
+  computed: {
+    addToA: function () {
+      console.log('addToA');
+      return this.a + this.age;
+    },
+    addToB: function () {
+      console.log('addToB');
+      return this.b + this.age;
+    }
+  }
+});
