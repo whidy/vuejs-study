e0e97e27f81ec91ef78c314153e2c93009e0869f
ShunBai <shunbai@infinite.net.cn>
2017-11-22 14:40:29 +0800

    submit done

--- app.js
+++ app.js
@@ -1,69 +1,23 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: '',
-    job: 'fe',
-    website: 'http://www.whidy.net',
-    websiteTag: '<a href="http://www.whidy.net">my blog(output by v-html)</a>',
-    age: 20,
-    x: 0,
-    y: 0,
-    a: 0,
-    b: 0,
-    error: false,
-    success: false,
-    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-    ninjas: [{
-        name: 'Ryu',
-        age: 25
-      },
-      {
-        name: 'Yoshi',
-        age: 35
-      },
-      {
-        name: 'Ken',
-        age: 55
-      }
-    ]
+    health: 100,
+    ended: false
   },
   methods: {
-    greet: function (time) {
-      return 'Good ' + time + ' ' + this.name;
+    punch: function () {
+      this.health -= Math.random() * 10;
+      if (this.health <= 0) {
+        this.health = 0;
+        this.ended = true;
+      }
     },
-    add: function (inc) {
-      this.age += inc;
-    },
-    subtract: function (dec) {
-      this.age -= dec;
-    },
-    updateXY: function (event) {
-      // console.log(event);
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    logName: function () {
-      // console.log('you entered your name');
-      // this.name = 
-    },
-    logAge: function () {
-      console.log('you entered your age');
-    },
-    // addToA: function () {
-    //   return this.a + this.age;
-    // },
-    // addToB: function () {
-    //   return this.b + this.age;
-    // }
+    restart: function () {
+      this.health = 100;
+      this.ended = false;
+    }
   },
   computed: {
-    addToA: function () {
-      console.log('addToA');
-      return this.a + this.age;
-    },
-    addToB: function () {
-      console.log('addToB');
-      return this.b + this.age;
-    }
+
   }
 });
